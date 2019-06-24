6e9639e7cc66de86e1251b354d7cff1f2be0733f
Luiz Paulo Franz <luizpaulofranz@gmail.com>
2019-06-23 23:55:44 -0300

    adds Feed listing page api consuming and styling.

--- instarocket/src/pages/Feed.js
+++ instarocket/src/pages/Feed.js
@@ -1,7 +1,13 @@
 import React, { Component } from 'react';
 
-import { View, Image, TouchableOpacity } from 'react-native';
+import { View, Text, Image, TouchableOpacity, FlatList, StyleSheet } from 'react-native';
+import api from '../services/api';
+
 import camera from '../assets/camera.png';
+import more from '../assets/more.png';
+import like from '../assets/like.png';
+import comment from '../assets/comment.png';
+import send from '../assets/send.png';
 
 // import { Container } from './styles';
 
@@ -17,7 +23,124 @@ export default class Feed extends Component {
     )
   })
 
+
+  state = {
+      feed: []
+  }
+
+  async componentDidMount() {
+      //this.registerToSocket();
+
+      const response = await api.get('posts');
+      //console.log(response.data);
+      this.setState({feed: response.data});
+  }
+
   render() {
-    return <View />;
+    return (
+     <View style={styles.container}>
+       <FlatList 
+        data={this.state.feed} 
+        keyExtractor={post => post._id}
+        renderItem={ ( {item} ) => (
+          <View style={styles.feedItem}>
+
+            <View style={styles.feedItemHeader}>
+              <View style={styles.userInfo}>
+                <Text style={styles.name}>{item.author}</Text>
+                <Text style={styles.place}>{item.place}</Text>
+              </View>
+
+              <Image source={more} />
+            </View>
+
+            <Image style={styles.feedImage} source={{uri: `http://10.0.3.2:3333/files/${item.image}`}} />
+
+            <View style={styles.feedItemFooter}>
+              <View style={styles.actions}> 
+                <TouchableOpacity style={styles.action} onPress={() => {}}>
+                  <Image source={like} />
+                </TouchableOpacity>
+                <TouchableOpacity style={styles.action} onPress={() => {}}>
+                  <Image source={comment} />
+                </TouchableOpacity>
+                <TouchableOpacity style={styles.action} onPress={() => {}}>
+                  <Image source={send} />
+                </TouchableOpacity>
+              </View>
+
+              <Text style={styles.likes}>{item.likes} curtidas</Text>
+              <Text style={styles.description}>{item.description}</Text>
+              <Text style={styles.hashtags}>{item.hashtags}</Text>
+
+            </View>
+
+          </View>
+        ) }
+      />
+
+    </View>
+    );
   }
 }
+
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1
+  },
+  feedItem: {
+    marginTop: 20
+  },
+  feedItemHeader: {
+    paddingHorizontal: 15,
+
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'center'
+  },
+  name: {
+    fontSize: 14,
+    color: '#000'
+  },
+
+  name: {
+    fontSize: 14,
+    color: '#000'
+  },
+  place: {
+    fontSize: 12,
+    color: '#666'
+  },
+
+  feedImage: {
+    width: '100%',
+    height: 400,
+    marginVertical: 15
+  },
+
+  feedItemFooter: {
+    paddingHorizontal: 15
+  },
+
+  actions: {
+    flexDirection: 'row'
+  },
+  action: {
+    marginRight: 8
+  },
+
+  likes: {
+    marginTop: 15,
+    fontWeight: 'bold',
+    color: '#000'
+  },
+  description: {
+    lineHeight: 18,
+    color: '#000'
+  },
+
+  hastags: {
+    color: '#7159c1'
+  }
+});
