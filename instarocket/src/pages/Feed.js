c01e790e3881853fa4114cff5b02cdacbe53ec53
Luiz Paulo Franz <luizpaulofranz@gmail.com>
2019-06-23 23:10:38 -0300

    Adds assets and default simple navigations.

--- instarocket/src/pages/Feed.js
+++ instarocket/src/pages/Feed.js
@@ -1,10 +1,22 @@
 import React, { Component } from 'react';
 
-import { View } from 'react-native';
+import { View, Image, TouchableOpacity } from 'react-native';
+import camera from '../assets/camera.png';
 
 // import { Container } from './styles';
 
 export default class Feed extends Component {
+  // this is a default var to config the navigation ops of this page
+  // we have access to some properties in this function
+  static navigationOptions = ({ navigation }) => ({
+    headerRight: (
+      // we pass the route to navigation
+      <TouchableOpacity style={{marginRight: 20}} onPress={ () => navigation.navigate('New') } >
+        <Image source={camera} />
+      </TouchableOpacity>
+    )
+  })
+
   render() {
     return <View />;
   }
